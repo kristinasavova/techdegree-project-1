4d6e5259890783fe9aa93592c4dd9a7d2f1f168d
Kristina <ccthw@protonmail.com>
2020-04-15 22:29:47 +0300

    Add more quotes, update script.js

---
+++ js/quotes.js
@@ -0,0 +1,170 @@
+/* This is an array of objects. Each object contains a quote and a source of the quote. 
+Some objects contain also citation, year and tags which can be related to the quote. */
+
+let quotes = [
+    {
+        quote: "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
+        source: "J.K. Rowling, Harry Potter and the Sorcerer's Stone",
+        citation: "Albus Dumbledore",  
+        year: "1997",
+        tags: "human nature"
+    },
+    {
+        quote: "Dobby is free!",
+        source: "J.K. Rowling, Harry Potter and the Chamber of Secrets", 
+        citation: "Dobby",
+        year: "1998"
+    },
+    {
+        quote: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+        source: "J.K. Rowling, Harry Potter and the Goblet of Fire",
+        citation: "Sirius Black", 
+        year: "2000",
+        tags: "human nature"
+    },
+    {
+        quote: "Everyone has secrets. It's just a matter of finding out what they are.",
+        source: "Stieg Larsson, The Girl with the Dragon Tattoo",
+        citation: "Lisbeth Salander", 
+        year: "2011",
+        tags: "human nature"
+    },
+    {
+        quote: "Why is it that when one man builds a wall, the next man immediately needs to know what's on the other side?",
+        source: "George R. R. Martin, Game of Thrones",
+        tags: "human nature"
+    }, 
+    {
+        quote: "Winter is coming...",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Ned Stark" 
+    },
+    {
+        quote: "The night is dark and full of terrors.", 
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Melisandre"
+    }, 
+    {
+        quote: "Fear cuts deeper than swords.",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Arya Stark",
+        tags: "human nature"
+    },
+    {
+        quote: "Rhaegar fought valiantly, Rhaegar fought nobly, Rhaegar fought honorably. And Rhaegar died.",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Jorah Mormont"
+    },
+    {
+        quote: "Never trust anything that can think for itself if you can’t see where it keeps its brain.",
+        source: "J.K. Rowling, Harry Potter and the Chamber of Secrets",
+        citation: "Arthur Weasley",
+        year: "1998"
+    },
+    {
+        quote: "Differences of habit and language are nothing at all if our aims are identical and our hearts are open.",
+        source: "J.K. Rowling, Harry Potter and the Goblet of Fire",
+        citation: "Albus Dumbledore",
+        year: "2000",
+        tags: "human nature"
+    }, 
+    {
+        quote: "What do I care how he looks? I am good-looking enough for both of us, I think! All these scars show is at my husband is brave!",
+        source: "J.K. Rowling, Harry Potter and the Half-Blood Prince",
+        citation: "Fleur Delacour",
+        tags: "love"
+    },
+    {
+        quote: "Love is the death of duty.",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Jon Snow",
+        tags: "love" 
+    },
+    {
+        quote: "Everything before the word ‘but’ is horseshit.",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Jon Snow" 
+    },
+    {
+        quote: "The man who passes the sentence should swing the sword.",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Ned Stark", 
+        tags: "noble"
+    },
+    {
+        quote: "When you play the game of thrones, you win or you die.",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Cersei Lannister"
+    },
+    {
+        quote: "Never forget what you are. The rest of the world will not. Wear it like armour, and it can never be used to hurt you.",
+        source: "George R. R. Martin, Game of Thrones",
+        citation: "Tyrion Lannister",
+        tags: "human nature"
+    },
+    {
+        quote: "Annushka has already bought the sunflower oil, and has not only bought it, but has already spilled it.",
+        source: "Mikhail Bulgakov, The Master and Margarita",
+        citation: "Woland", 
+        year: "1928-1940"
+    },
+    {
+        quote: "You should never ask anyone for anything. Never- and especially from those who are more powerful than yourself. They will make the offer and they will give of their own accord.",
+        source: "Mikhail Bulgakov, The Master and Margarita",
+        citation: "Woland", 
+        year: "1928-1940"
+    },
+    {
+        quote: "Respect was invented to cover the empty place where love should be. But if you don’t love me, it would be better to say so.",
+        source: "Leo Tolstoy, Anna Karenina",
+        citation: "Anna Karenina", 
+        year: "1877",
+        tags: "love"
+    },
+    {
+        quote: "All happy families are alike; each unhappy family is unhappy in its own way.",
+        source: "Leo Tolstoy, Anna Karenina",
+        citation: "Leo Tolstoy", 
+        year: "1877",
+        tags: "human nature"
+    }, 
+    {
+        quote: "Dear Scarlett! You aren’t helpless. Anyone as selfish and determined as you are is never helpless.",
+        source: "Margaret Mitchell, Gone with the Wind",
+        citation: "Rhett Butler",
+        year: "1936",
+        tags: "human nature" 
+    },
+    {
+        quote: "Abandon all hope, ye who enter here.",
+        source: "Dante, Inferno",
+        citation: "Dante Alighieri",
+        year: "14th century"
+    },
+    {
+        quote: "From the day I plunged you in the river to give you life, I loved you as I loved the steppes. You were my pride! I gave you life. It is on me to take it away from you.",
+        source: "Nikolai Gogol, Taras Bulba",
+        citation: "Taras Bulba",
+        year: "1835"
+    },
+    {
+        quote: "I read that every known superstition in the world is gathered into the horseshoe of the Carpathians, as if it were the centre of some sort of imaginative whirlpool; if so my stay may be very interesting.",
+        source: "Bram Stoker, Dracula",
+        citation: "Jonathan Harker",
+        year: "1897"
+    }, 
+    {
+        quote: "What she had realized was that love was that moment when your heart was about to burst.",
+        source: "Stieg Larsson, The Girl with the Dragon Tattoo",
+        citation: "Stieg Larsson", 
+        year: "2011",
+        tags: "love"
+    },
+    {
+        quote: "Friendship- my definition- is built on two things. Respect and trust. Both elements have to be there. And it has to be mutual. You can have respect for someone, but if you don't have trust, the friendship will crumble.",
+        source: "Stieg Larsson, The Girl with the Dragon Tattoo",
+        citation: "Mikael Blomkvist", 
+        year: "2011",
+        tags: "friendship"
+    }
+  ];
--- js/script.js
+++ js/script.js
@@ -1,108 +1,55 @@
 // Treehouse FSJS Techdegree: Project 1 - A Random Quote Generator
 
-/* This is an array of objects. Each object contains a quote and a source of the quote. 
-Some objects contain also citation, year and tags which can be related to the quote. */
-
-let quotes = [
-  {
-    quote: "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
-    source: "Harry Potter and the Sorcerer's Stone",
-    citation: "Albus Dumbledore",  
-    year: 1997,
-    tags: "human nature"
-  },
-  {
-    quote: "Dobby is free.",
-    source: "Harry Potter and the Chamber of Secrets", 
-    citation: "Dobby",
-    year: 1998
-  },
-  {
-    quote: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
-    source: "Harry Potter and the Goblet of Fire",
-    citation: "Sirius Black", 
-    year: 2000,
-    tags: "human nature"
-  },
-  {
-    quote: "Always", 
-    source: "Harry Potter and the Deathly Hallows", 
-    citation: "Severus Snape", 
-    year: 2007,
-    tags: "love"
-  },
-  {
-    quote: "Everyone has secrets. It's just a matter of finding out what they are.",
-    source: "The Girl with the Dragon Tattoo",
-    citation: "Lisbeth Salander", 
-    year: 2011,
-    tags: "human nature"
-  },
-  {
-    quote: "Why is it that when one man builds a wall, the next man immediately needs to know what's on the other side?",
-    source: "A Game of Thrones",
-    tags: "human nature"
-  }, 
-  {
-    quote: "Winter is coming.",
-    source: "A Game of Thrones"
-  },
-  {
-    quote: "The night is dark and full of terrors.", 
-    source: "A Game of Thrones",
-    citation: "Melisandre"
-  }
-]
-
 /* A getRandomQuote function returns a random quote object using Math.random function to provide a random number 
 and Math.floor function to round the reuslt of Math.random function downward to its nearest interger.*/
- 
-function getRandomQuote() {
-  let quoteNumber = Math.floor(Math.random() * quotes.length); 
-  return quotes[quoteNumber];
-}
 
+function getRandomQuote () {
+    const quoteNumber = Math.floor (Math.random () * quotes.length); 
+    return quotes[quoteNumber];
+}
+  
 /* These two functions update the quote automatically every 10 seconds. */
 
-let timer = setInterval(printQuote, 10000);
+let timer = setInterval (printQuote, 10000);
 
-function startInterval() {
-  timer = setInterval(printQuote, 10000);
+function startInterval () {
+    timer = setInterval (printQuote, 10000);
 }
-function stopInterval() {
-  timer = clearInterval(timer);
+
+function stopInterval () {
+    timer = clearInterval (timer);
 }
 
 /* This is a function that updates the background color to a random color each time when a new quote prints to the page. */
 
-function changeBackground() {
-  let col = `rgb(${Math.floor(Math.random()* 255)}, ${Math.floor(Math.random()* 255)}, ${Math.floor(Math.random()* 255)})`;
-  for (let i = 0; i < quotes.length; i ++) {
-    document.body.style.backgroundColor = col;
-  }
+function changeBackground () {
+    let col = `rgb(${Math.floor(Math.random()* 255)}, ${Math.floor(Math.random()* 255)}, ${Math.floor(Math.random()* 255)})`;
+    for (let i = 0; i < quotes.length; i ++) {
+        document.body.style.backgroundColor = col;
+    }
 }
 
 /* This is a function that prints a quote. It uncludes three conditional statements, which add citations, 
 years and tags to the quotes in case if there are these kinds of information in the array of quotes.*/
 
-function printQuote() {
-  let randomQuote = getRandomQuote();
-  let htmlString = `<p class="quote">${randomQuote.quote}</p>`;
-  htmlString += `<p class="source">${randomQuote.source} `;
-  if (randomQuote.citation) {
-    htmlString += `<span class="citation">${randomQuote.citation} </span>`;
-  }
-  if (randomQuote.year) {
-    htmlString += `<span class="year">${randomQuote.year}</span>`;  
-  } 
-  if (randomQuote.tags) {
-    htmlString += `<p>${randomQuote.tags}`;
-  }
-  htmlString += `</p>`;
-  document.getElementById('quote-box').innerHTML = htmlString; 
-  changeBackground();
-  startInterval();
-  stopInterval();
+function printQuote () {
+    let randomQuote = getRandomQuote ();
+    let htmlString = `<p class="quote">${randomQuote.quote}</p>`;
+    htmlString += `<p class="source">${randomQuote.source} `;
+    if (randomQuote.citation) {
+      htmlString += `<span class="citation">${randomQuote.citation} </span>`;
+    }
+    if (randomQuote.year) {
+      htmlString += `<span class="year">${randomQuote.year}</span>`;  
+    } 
+    if (randomQuote.tags) {
+      htmlString += `<p>${randomQuote.tags}`;
+    }
+    htmlString += `</p>`;
+    document.getElementById('quote-box').innerHTML = htmlString; 
+    changeBackground ();
+    startInterval ();
+    stopInterval ();
 }
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById ('load-quote').addEventListener ("click", printQuote, false);
